3e885d713622ba92dce54a92c2bcf9e950026655
Stefan Gamerith <steff7@gmx.com>
2012-12-23 13:06:03 +0100

    Deleted tu_fp_custom.js and moved it to tu_fp.js

--- js/tu_fp/tu_fp.js
+++ js/tu_fp/tu_fp.js
@@ -87,6 +87,20 @@ function dispDate(now){
 	}
 }
 
+function move(f, t)
+{
+     var i = f.options.length;
+     while(i--)
+     {
+        if(f.options[i].selected)
+        {
+             t.options[t.options.length] = f.options[i];
+             f.options[i] = null;
+        }
+     }
+
+}
+
 var showStatisticBars = function() {
 	$(".statisticsbarup").slideDown(350);
 	$(".statisticsbardown").slideDown(350);
